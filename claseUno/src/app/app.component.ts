23f0a79a23cf340c22b5bc79a9e388602edeae40
julianRodriguez <julian_rodriguez99@hotmail.com>
2024-04-20 20:52:32 -0300

    update

--- claseUno/src/app/app.component.ts
+++ claseUno/src/app/app.component.ts
@@ -24,12 +24,4 @@ export class AppComponent {
     this.edadDos="0";
     this.edadUno="0";
   }
-
 }
-/*
-1- (app.component.html) Realizar:
-  Una aplicación que se le ingresen dos edades en dos cuadro de textos
-  edadUno, EdadDos
-  mostrar el promedio y la suma en dos cuadros de textos nuevos
-  botones "calcular" y "limpiar cuadros de textos"
-*/ 
