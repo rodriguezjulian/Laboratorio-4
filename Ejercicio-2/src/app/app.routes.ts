b2050baf21f10b968aec5e827ac3950e30665fa6
julianRodriguez <julian_rodriguez99@hotmail.com>
2024-04-21 00:52:49 -0300

    update

---
+++ Ejercicio-2/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { BienvenidaComponent } from './components/bienvenida/bienvenida.component';
+export const routes: Routes = [
+    {
+        path: "bienvenida",
+        component : BienvenidaComponent 
+    }
+];
