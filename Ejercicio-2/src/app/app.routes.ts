bca7edf94e5bad13df0c4eecc569e1f60c0c9fa6
julianRodriguez <julian_rodriguez99@hotmail.com>
2024-04-21 01:42:33 -0300

    update

--- Ejercicio-2/src/app/app.routes.ts
+++ Ejercicio-2/src/app/app.routes.ts
@@ -1,8 +1,30 @@
 import { Routes } from '@angular/router';
 import { BienvenidaComponent } from './components/bienvenida/bienvenida.component';
+import { LoginComponent } from './components/login/login.component';
+import { ErrorComponent } from './components/error/error.component';
 export const routes: Routes = [
     {
-        path: "bienvenida",
+        path: "",
         component : BienvenidaComponent 
+    },
+    {
+        path: "login",
+        component : LoginComponent
+    },
+    {
+        path:"error",
+        component: ErrorComponent
+    },
+    {
+        path : "bienvenido",
+        redirectTo : ""
+    },
+    {
+        path : "bienvenida",
+        redirectTo : ""
+    },
+    {
+        path : "**",
+        component: ErrorComponent
     }
 ];
--- Ejercicio-2/src/app/components/bienvenida/bienvenida.component.ts
+++ Ejercicio-2/src/app/components/bienvenida/bienvenida.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { RouterLink, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-bienvenida',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './bienvenida.component.html',
   styleUrl: './bienvenida.component.css'
 })
